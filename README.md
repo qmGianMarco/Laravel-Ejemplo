25dabee4d2110a84c364bcb2fe0e0de593283fa0
qmGianMarco <gian30marco12@gmail.com>
2024-07-19 10:04:33 -0500

    manejo de rutas

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Laravel-Ejemplo
 En este repositorio se realizaran practicas y ejemplos para aprender Laravel
+
+# CARPETAS
+## routes
+Sirve para crear las rutas y asignarles vistas(views)
+Las rutas se leen de arriba hacia abajo
+![alt text](image.png)
+### peticiones
+-GET: Sirve para que el usurio pida recursos (redireccionar a una pagina)
+-POST: Sirve para enviar recursos
+-PUT: Sirve actualizar registros
+-PATCH: Sirve actualizar registros
+-DELETE: Sirve eliminar registros
+
+## views
+Sirve para crear las vistas para el usuario
+
+# ARCHIVOS
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -3,5 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    /* return "Welcome to hompage"; */
     return view('welcome');
+});
+
+Route::get('/posts', function () {
+    return "Aqui se mostraran todos los posts";
+});
+
+/* RUTAS CON VARIABLES */
+/* Route::get('/posts/{post}', function ($post) {
+    return "Aqui se mostrara el post {$post}";
+}); */
+
+Route::get('/posts/{post}/{category?}', function ($post, $category = null) {
+    if ($category) {
+        return "Aqui se mostrara el post {$post} de la categoria {$category}";
+    } else {
+        return "Aqui se mostrara el post {$post}";
+    }
+    
 });
